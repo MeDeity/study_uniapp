99889fd53d88b86782b1d5fe5e45ccb0485b46c4
fwenhua <langrenbule@gmail.com>
2019-11-29 18:09:25 +0800

    首页UI布局

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,9 +4,35 @@
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view> -->
+		
+		<view class="index-list">
+			<view class="index-list1">
+				<view>
+					<image src="../../static/mine_select.png" mode="widthFix" lazy-load="" ></image>
+					昵称
+				</view>
+				<view>
+					<view class="icon iconfont icon-bofang"></view>关注
+				</view>
+			</view>
+			<view class="index-list2">我是标题</view>
+			<view class="index-list3"></view>
+				<image src="../../static/logo.png" mode="widthFix"></image>
+			<view class="index-list4">
+				<view>
+					<view><view class="icon iconfont icon-bofang"></view>10</view>
+					<view><view class="icon iconfont icon-bofang"></view>10</view>
+				</view>
+				<view>
+					<view><view class="icon iconfont icon-bofang"></view>10</view>
+					<view><view class="icon iconfont icon-bofang"></view>10</view>
+				</view>
+			</view>
+		</view>
 		<navigator url="../ceshi/ceshi">
 			<view class="icon iconfont icon-bofang" style="color: #09BB07;">播放</view>
-			<view class="">跳转到动画测试页</view>
+			
+			
 		</navigator>
 		
 	</view>
@@ -53,5 +79,73 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+
+	.index-list{
+		padding: 20upx;
+		border-bottom: 1upx solid #999999;
+	}
+	.index-list1{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.index-list1>view:first-child{
+		align-items: center;
+		color: #999999;
+	}
+	
+	.index-list1>view:first-child image{
+		width: 90upx;
+		height: 90upx;
+		border-radius: 100%;
+		margin-right: 10upx;
+		align-content: center;
+		align-items: center;
+	}
+	.index-list1>view:first-child{
+		display: flex;
+		align-content: center;
+	}
+	.index-list1>view:last-child{
+		display: flex;
+		align-items: center;
+		padding-left: 15upx;
+		padding-right: 15upx;
+	}
+	.index-list1>view:last-child view{
+		margin-right: 10upx;
+	}
+	
+	.index-list1>view:last-child{
+		background-color: #929292;
+		border-radius: 5upx;
+	}
+	
+	.index-list2{
+		
+	}
+	.index-list3{
+		width: 100%;
+		border-radius: 20upx;
+	}
+	.index-list4{
+		display: flex;
+		justify-content: space-between;
+		align-content: center;
+		padding: 15upx;
+	}
+	.index-list4>view:first-child{
+		display: flex;
+		align-content: center;
+	}
+	.index-list4>view:last-child{
+		display: flex;
+		align-content: center;
+	}
+	.index-list4>view>view{
+		display: flex;
+		align-items: center;
+		padding: 10upx;
 	}
 </style>
